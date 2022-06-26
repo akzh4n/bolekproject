c7199e8a89aedf0a2f846b21ee0f0d8c368fa95a
manste1n <akzhan.kaz2003@gmail.com>
2022-06-26 07:00:27 -0700

    wow

---
+++ app/src/main/java/com/example/bolekproject/Capture.java
@@ -0,0 +1,7 @@
+package com.example.bolekproject;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class Capture extends CaptureActivity {
+
+}
--- app/src/main/java/com/example/bolekproject/QRActivity.java
+++ app/src/main/java/com/example/bolekproject/QRActivity.java
@@ -1,23 +1,30 @@
 package com.example.bolekproject;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class QRActivity extends AppCompatActivity {
+    private ImageView qrcodeImage;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qractivity);
-
-
-
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
@@ -44,5 +51,44 @@ public class QRActivity extends AppCompatActivity {
                 return  false;
             }
         });
+
+        qrcodeImage = (ImageView) findViewById(R.id.qrcodeimage);
+
+        qrcodeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(QRActivity.this);
+                intentIntegrator.setPrompt("Для включения вспышки нажмите кнопки увеличения громкости!");
+                intentIntegrator.setBeepEnabled(true);
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setCaptureActivity(Capture.class);
+                intentIntegrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (intentResult.getContents() != null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(QRActivity.this);
+            builder.setTitle("Результат:");
+            builder.setMessage(intentResult.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+
+            builder.show();
+
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Извините пройзошла ошибка, попробуйте пересканировать!", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
